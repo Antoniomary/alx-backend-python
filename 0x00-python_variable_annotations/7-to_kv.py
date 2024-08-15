b3817164f022eef5d36f1954502a010930cce7ec
Antoniomary <amanso.totti@yahoo.com>
2024-08-15 14:14:18 +0000

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """contains to_kv function"""
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: [int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """a function that returns a tuple"""
     return (k, v ** 2)
