8b59937778941c6748a81fe7805f22e2c829faa9
Antoniomary <amanso.totti@yahoo.com>
2024-08-15 22:07:42 +0000

    101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """contains safely_get_value function"""
-from typing import Any, Mapping, Optional, TypeVar
+from typing import Any, Mapping, Union, TypeVar
 
 
-T = TypeVar('T')
+T = TypeVar('T', int, str, float)
 
 
 def safely_get_value(dct: Mapping,
                      key: Any,
-                     default: Optional[T] = None) -> Optional[T]:
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """a function that returns the value of a key in a dictionary if key is
        present else returns the value in default which defaults to None
     """
