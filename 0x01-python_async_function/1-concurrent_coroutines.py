a49330bc5feba65ea70cba76b5dd881a3ab88570
Antoniomary <amanso.totti@yahoo.com>
2024-08-19 17:51:19 +0000

    1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""contains wait_n async function"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """spawn wait_random n times and returns list of all the delays"""
+    delays: list = []
+    for i in range(n):
+        delay: float = await wait_random(max_delay)
+        if not delays:
+            delays.append(delay)
+        else:
+            j: int = 0
+            while j < len(delays) and delays[j] < delay:
+                j += 1
+            delays.insert(j, delay)
+    return delays
