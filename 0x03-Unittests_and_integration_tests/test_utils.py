f5638dbbd5d1fcbc04f9b64cbaaf83253576ccd4
Antoniomary <amanso.totti@yahoo.com>
2024-09-09 01:17:09 +0000

    added test_access_nested_map_exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,7 +12,7 @@ import utils
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """test for utils.access_nested_map
+    """test cases
     """
     @parameterized.expand([
             ({"a": 1}, ("a",), 1),
@@ -25,6 +25,19 @@ class TestAccessNestedMap(unittest.TestCase):
         """test the return value of utils.access_nested_map
         """
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+            ({}, ("a",), "KeyError: 'a'"),
+            ({"a": 1}, ("a", "b"), "KeyError: 'b'")
+    ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping, path: Sequence,
+                                         expected):
+        """tests for exceptions in utils.access_nested_map
+        """
+        with self.assertRaises(KeyError) as cm:
+            utils.access_nested_map(nested_map, path)
+            self.assertEqual(cm.exception, expected)
 
 
 if __name__ == "__main__":
