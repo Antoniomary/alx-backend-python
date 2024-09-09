41eb5ba298635882ae7868a94ef24fa3e463c76d
Antoniomary <amanso.totti@yahoo.com>
2024-09-09 13:05:14 +0000

    added test_memoize test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from typing import (
         Sequence
 )
 from unittest.mock import patch
+from utils import memoize
 import unittest
 import utils
 
@@ -56,6 +57,38 @@ class TestGetJson(unittest.TestCase):
         result = utils.get_json(test_url)
         self.assertEqual(result, test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """test cases
+    """
+    def test_memoize(self):
+        """test for memoization
+        """
+        class TestClass:
+            """Class to test memoization
+            """
+            def a_method(self):
+                """returns 42
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """return a_method
+                """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as cm:
+            my_class = TestClass()
+
+            result1 = my_class.a_property
+            result2 = my_class.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            cm.assert_called_once()
 
 
 if __name__ == "__main__":
